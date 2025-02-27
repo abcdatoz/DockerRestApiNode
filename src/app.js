fddc9e1102ad19cbba5e0e58d60f9cc36b44023c
rey <rhernandez@orfis.gob.mx>
2025-02-27 12:25:26 -0600

    last commit

--- src/app.js
+++ src/app.js
@@ -6,3 +6,37 @@ app.use(morgan("dev"));
 app.use(require("./routes/index"));
 
 module.exports = app;
+
+/*
+
+	• Mkdir projectName
+	• Cd projectName
+	• Npm init
+	• Npm I express
+	• Mkdir src
+		○ Touch index.js
+		○ Touch app.js
+	• Mkdir src/routes
+	• Touch src/routes/index.js
+	• Add code
+	• Initialize docker desktop
+
+		○ Link to github repo
+
+	• Touch Dockerfile
+		○ Fill it
+		
+	• Touch .dockerignore
+		○ Fill it
+	• docker buildx build -t  node-restapi .
+		○ I have to download an image from dockerwindowsApp previous to run this
+		
+	• docker images
+	• docker run -it -p 4404:3303 noderestapi
+	• docker ps  //para ver los procesos corriendo
+	• Docker stop noderestapi
+
+FaztCode
+https://www.youtube.com/watch?v=iLlmm0L-VpQ  
+
+*/
